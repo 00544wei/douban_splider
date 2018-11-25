dbf6b2c5a651fa2d10a2f6960f1e5342ae741009
zhangwei <1358235270@qq.com>
2018-11-25 20:06:05 +0800

    Initial commit

---
+++ douban_splider/items.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DoubanSpliderItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    #序号
+    serial_number = scrapy.Field()
+    #电影的名称
+    movie_name = scrapy.Field()
+    #电影的介绍
+    introduce = scrapy.Field()
+    #电影的星级
+    start = scrapy.Field()
+    电影的评级
+    evaluate = scrapy.Field()
+    #电影的描述
+    description = scrapy.Field()
+    pass
