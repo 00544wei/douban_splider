46b0b543852155d4a205dbc6407f8f3972b73bf7
zhangwei <1358235270@qq.com>
2018-11-26 22:57:03 +0800

    zw
    zw

--- douban_splider/main.py
+++ douban_splider/main.py
@@ -1,4 +1,5 @@
 # __*__ coding: utf-8 __*__
 from scrapy import cmdline
 
-cmdline.execute('scrapy crawl douban_splider -o douban.csv'.split())
+# cmdline.execute('scrapy crawl douban_splider -o douban.csv'.split())
+cmdline.execute('scrapy crawl douban_splider'.split())
